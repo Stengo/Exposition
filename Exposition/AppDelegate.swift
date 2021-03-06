983c362442a2f28059a20c7276697d9cd8dc9ae1
Stengo <bastian.andelefski@googlemail.com>
2021-03-06 09:31:14 +0100

    Initial Commit

---
+++ Exposition/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Exposition
+//
+//  Created by Bastian Andelefski on 06.03.21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
