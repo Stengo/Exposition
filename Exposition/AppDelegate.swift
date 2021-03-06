2b9a10841cf5b9ec0e9bc31d126c77fbe86cd38b
Stengo <bastian.andelefski@googlemail.com>
2021-03-06 13:20:14 +0100

    Turns the project into a status bar app

--- Exposition/AppDelegate.swift
+++ Exposition/AppDelegate.swift
@@ -9,6 +9,23 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
+    let statusItem: NSStatusItem = {
+        let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+
+        statusItem.button?.image = NSImage(named:NSImage.Name("statusBarIcon"))
+
+        let menu = NSMenu()
+        menu.addItem(
+            NSMenuItem(
+                title: "Quit Exposition",
+                action: #selector(NSApplication.terminate),
+                keyEquivalent: ""
+            )
+        )
+        statusItem.menu = menu
+
+        return statusItem
+    }()
 
     @IBOutlet var window: NSWindow!
 
